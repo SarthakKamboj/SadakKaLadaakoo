882321b3b8298dc7da83ffa7ca91d55e1a548487
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-10-27 23:16:09 -0700

    added logic to create graphics pipeline states and vertex/pixel shaders, create command lists, and vertex buffers

--- src/gfx_api/skl_d3d12.cpp
+++ src/gfx_api/skl_d3d12.cpp
@@ -1,11 +1,13 @@
 #include "skl_d3d12.h"
 
-#include <D3d12.h>
 #include <d3dx12.h>
+
+#include <D3d12.h>
 #include <D3d12SDKLayers.h>
 #include <dxgi.h>
 #include <dxgi1_2.h>
 #include <dxgi1_4.h>
+#include <d3dcompiler.h>
 #include <wrl.h>
 
 #include "defines.h"
@@ -90,8 +92,8 @@ void initD3D12(HWND hwnd) {
   }
   int frameIndex = swapChain3->GetCurrentBackBufferIndex();
 
-  // WARNING: learn about what descriptors actually are
-  // create the heap of descriptors for the render target views
+  // descriptors/views describe Id3d resource objects (in this case a render target) 
+  // a render target view is a type of descriptor for the render target
   ComPtr<ID3D12DescriptorHeap> descriptorHeap;
   D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
   heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
@@ -115,6 +117,7 @@ void initD3D12(HWND hwnd) {
     }
 
     D3D12_RENDER_TARGET_VIEW_DESC rtvDesc = {};
+    // create a render target out of the swap chain buffer and associate it with the render target view
     device->CreateRenderTargetView(renderTargets[i].Get(), NULL, rtvHandle);
     rtvHandle.Offset(1, rtvDescHandleSize);
   }
@@ -125,6 +128,136 @@ void initD3D12(HWND hwnd) {
     SKL_LOG("could not create command allocator");
     return;
   }
+
+  D3D12_ROOT_SIGNATURE_DESC rootSigDesc{};
+  rootSigDesc.NumParameters = 0;
+  rootSigDesc.pParameters = NULL;
+  rootSigDesc.NumStaticSamplers = 0;
+  rootSigDesc.pStaticSamplers = NULL;
+  rootSigDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+  ComPtr<ID3DBlob> serializedSig;
+  ComPtr<ID3DBlob> error;
+  if (S_OK != D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1, &serializedSig, &error)) {
+    SKL_LOG("could not serialize root signature");
+    return;
+  }
+  ComPtr<ID3D12RootSignature> rootSig;
+  if (S_OK != device->CreateRootSignature(0, serializedSig->GetBufferPointer(), serializedSig->GetBufferSize(), IID_PPV_ARGS(&rootSig))) {
+    SKL_LOG("could not create root signature on device");
+    return;
+  }
+
+  ComPtr<ID3DBlob> vertexShader;
+  ComPtr<ID3DBlob> pixelShader;
+  const wchar_t* fileName = L"src\\shaders\\win\\shaders.glsl";
+  if (S_OK != D3DCompileFromFile(fileName, NULL, NULL, "VSMain", "vs_5_0", D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &vertexShader, NULL)) {
+    SKL_LOG("could not compile vertex shader");
+    return;
+  }
+
+  if (S_OK != D3DCompileFromFile(fileName, NULL, NULL, "PSMain", "ps_5_0", D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &pixelShader, NULL)) {
+    SKL_LOG("could not compile fragment shader");
+    return;
+  }
+
+  // seems like graphics pipeline state is very much tied to a vertex and index buffer layout
+  D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineStateDesc = {};
+  pipelineStateDesc.pRootSignature = rootSig.Get();
+  pipelineStateDesc.VS.pShaderBytecode = vertexShader->GetBufferPointer();
+  pipelineStateDesc.VS.BytecodeLength = vertexShader->GetBufferSize();
+  pipelineStateDesc.PS.pShaderBytecode = pixelShader->GetBufferPointer();
+  pipelineStateDesc.PS.BytecodeLength = pixelShader->GetBufferSize();
+  pipelineStateDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+  pipelineStateDesc.SampleMask = UINT_MAX;
+  pipelineStateDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+  pipelineStateDesc.DepthStencilState.DepthEnable = false;
+  pipelineStateDesc.DepthStencilState.StencilEnable = false;
+
+  D3D12_INPUT_ELEMENT_DESC inputAssemElemDesc[2] = {};
+  inputAssemElemDesc[0].SemanticName = "POSITION";
+  inputAssemElemDesc[0].SemanticIndex = 0;
+  inputAssemElemDesc[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+  inputAssemElemDesc[0].InputSlot = 0;
+  inputAssemElemDesc[0].AlignedByteOffset = 0;
+  inputAssemElemDesc[0].InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+  inputAssemElemDesc[0].InstanceDataStepRate = 0;
+
+  inputAssemElemDesc[1].SemanticName = "COLOR";
+  inputAssemElemDesc[1].SemanticIndex = 0;
+  inputAssemElemDesc[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT ;
+  inputAssemElemDesc[1].InputSlot = 0;
+  inputAssemElemDesc[1].AlignedByteOffset = 12;
+  inputAssemElemDesc[1].InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+  inputAssemElemDesc[1].InstanceDataStepRate = 0;
+
+  pipelineStateDesc.InputLayout.pInputElementDescs = inputAssemElemDesc;
+  pipelineStateDesc.InputLayout.NumElements = _countof(inputAssemElemDesc);
+
+  pipelineStateDesc.SampleDesc.Count = 1;
+  pipelineStateDesc.IBStripCutValue = D3D12_INDEX_BUFFER_STRIP_CUT_VALUE_DISABLED;
+  pipelineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+  pipelineStateDesc.NumRenderTargets = 1;
+  pipelineStateDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+
+  ComPtr<ID3D12PipelineState> pipelineState;
+  if (S_OK != device->CreateGraphicsPipelineState(&pipelineStateDesc, IID_PPV_ARGS(&pipelineState))) {
+    SKL_LOG("could not create graphics pipeline state object");
+    return;
+  }
+
+  ComPtr<ID3D12GraphicsCommandList> commandList;
+  if (S_OK != device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator.Get(), pipelineState.Get(), IID_PPV_ARGS(&commandList))) {
+    SKL_LOG("could not create command list");
+    return;
+  }
+
+  if (S_OK != commandList->Close()) {
+    SKL_LOG("could not close command list");
+    return;
+  }
+
+  SKL_Vertex verts[3] = {
+    { { 0.0f, 0.25f, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+    { { 0.25f, -0.25f, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+    { { -0.25f, -0.25f,  0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } },
+  };
+
+  int verticesSize = sizeof(SKL_Vertex) * 3;
+
+  D3D12_HEAP_PROPERTIES vertexBuffHeapProps = {};
+  vertexBuffHeapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
+  vertexBuffHeapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+  vertexBuffHeapProps.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+  vertexBuffHeapProps.CreationNodeMask = 0;
+
+  D3D12_RESOURCE_DESC vertBuffResourceDesc = {};
+  vertBuffResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+  // NOTE: why should alignment be 0 instead of 1?
+  vertBuffResourceDesc.Alignment = 0;
+  vertBuffResourceDesc.Width = verticesSize;
+  vertBuffResourceDesc.Height = 1;
+  vertBuffResourceDesc.DepthOrArraySize = 1;
+  vertBuffResourceDesc.MipLevels = 1;
+  vertBuffResourceDesc.Format = DXGI_FORMAT_UNKNOWN;
+  vertBuffResourceDesc.SampleDesc.Count = 1;
+  vertBuffResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR ;
+  vertBuffResourceDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+
+  ComPtr<ID3D12Resource> vertexBuffer;
+  if (S_OK != device->CreateCommittedResource(&vertexBuffHeapProps, D3D12_HEAP_FLAG_NONE, &vertBuffResourceDesc, 
+                                                D3D12_RESOURCE_STATE_GENERIC_READ, NULL, IID_PPV_ARGS(&vertexBuffer))) {
+    SKL_LOG("could not create the vertex buffer");
+    return;
+  }
+
+  void* vertexBufferDataPtr = NULL;
+  D3D12_RANGE readRange = {0,0};
+  if (S_OK != vertexBuffer->Map(0, &readRange, &vertexBufferDataPtr)) {
+    SKL_LOG("could not get the data ptr of the data referenced by the vertex buffer");
+  }
+  memcpy(vertexBufferDataPtr, verts, verticesSize);
+  vertexBuffer->Unmap(0, &readRange);
 
 
   SKL_LOG("successfully initialized D3d12");
--- src/gfx_api/skl_d3d12.h
+++ src/gfx_api/skl_d3d12.h
@@ -2,4 +2,53 @@
 
 #include <windows.h>
 
+
+struct SKL_Position {
+  float x = 0;
+  float y = 0;
+  float z = 0;
+
+#if false
+  SKL_Position() = default;
+
+  SKL_Position(float _x, float _y, float _z) {
+    x = _x;
+    y = _y;
+    z = _z;
+  }
+#endif
+};
+
+struct SKL_Color {
+  float r = 0;
+  float g = 0;
+  float b = 0;
+  float a = 1.0f;
+
+#if false
+  SKL_Color() = default;
+
+  SKL_Color(float _r, float _g, float _b, float _a) {
+    r = _r;
+    g = _g;
+    b = _b;
+    a = _a;
+  }
+#endif
+};
+
+struct SKL_Vertex {
+  SKL_Position pos;
+  SKL_Color color;
+
+#if false
+  SKL_Vertex() = default;
+
+  SKL_Vertex(SKL_Position _pos, SKL_Color _col) {
+    pos = _pos;
+    color = _col;
+  }
+#endif
+};
+
 void initD3D12(HWND hwnd);
